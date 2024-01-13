5db3e9f111a582083c2fbc3fcbfdc68b41b16d42
Boburjon <soliyevboburjon95@gmail.com>
2024-01-13 23:58:16 +0500

    I added function that protect xss attacks.

--- src/main/java/com/example/my_mvc_project/configurations/SecurityConfiguration.java
+++ src/main/java/com/example/my_mvc_project/configurations/SecurityConfiguration.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 @EnableMethodSecurity(
-        prePostEnabled = true,
         securedEnabled = true,
         jsr250Enabled = true
 )

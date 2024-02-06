645640587ff64cf599f15087868b4cdd6c230460
Boburjon <soliyevboburjon95@gmail.com>
2024-02-06 22:57:40 +0500

    I added thymeleaf security

--- src/test/java/com/example/my_mvc_project/MyMvcProjectApplicationTests.java
+++ src/test/java/com/example/my_mvc_project/MyMvcProjectApplicationTests.java
@@ -3,10 +3,6 @@ package com.example.my_mvc_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
-
 @SpringBootTest
 class MyMvcProjectApplicationTests {
 
